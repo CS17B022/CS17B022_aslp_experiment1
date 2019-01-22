dec49f66d87b7068aa10f0f31b84fa7946d4d302
CS17B022 <46784429+CS17B022@users.noreply.github.com>
2019-01-23 00:27:21 +0530

    Update lab1_1_comment.asm

--- lab1_1_comment.asm
+++ lab1_1_comment.asm
@@ -18,26 +18,26 @@ main:
 	mov	ebx, 10  			; ebx <- 10
 	mov	r12d, 20 			; r12d <- 20
 	lea	eax, [rbx+r12] 			; eax <- [rbx + r12] ; eax <- 30
-	mov	DWORD PTR [rbp-44], eax  	; [rbp-44] <- [eax]
+	mov	DWORD PTR [rbp-44], eax  	; rbp-44 <- [eax]
 	mov	eax, ebx  			; eax <- [ebx] ; eax <- 10
 	sub	eax, r12d  			; eax <- [eax] - [r12d] ; eax <- -10
-	mov	DWORD PTR [rbp-40], eax  	; [rbp -40] <- [eax]
+	mov	DWORD PTR [rbp-40], eax  	; rbp-40 <- [eax]
 	mov	eax, ebx  			; eax <- [ebx] ; eax <- 10
 	imul	eax, r12d  			; eax <- [eax] * [r12d] ; eax <- 200
-	mov	DWORD PTR [rbp-36], eax  	; [rbp-36] <- [eax]
+	mov	DWORD PTR [rbp-36], eax  	; rbp-36 <- [eax]
 	mov	eax, ebx  			; eax <- [ebx] ; eax <- 10
 	cdq					; copies sign bit (bit 31) of eax to every bit (bit 0 to bit 31) of edx
 	idiv	r12d  				; eax <- [eax] / [r12d] ; eax <- 0
-	mov	DWORD PTR [rbp-32], eax  	; [rbp-32] <- [eax]
+	mov	DWORD PTR [rbp-32], eax  	; rbp-32 <- [eax]
 	mov	eax, ebx ; eax <- [ebx]  	; eax <- 10
 	and	eax, r12d  			; eax <- [eax] & [r12d] ; eax <- 0
-	mov	DWORD PTR [rbp-28], eax  	; [rbp-28] <- [eax]
+	mov	DWORD PTR [rbp-28], eax  	; rbp-28 <- [eax]
 	mov	eax, r12d  			; eax <- [r12d] ; eax <- 20
 	or	eax, ebx  			; eax <- [eax] | [ebx] ; eax <- 30
-	mov	DWORD PTR [rbp-24], eax  	; [rbp-24] <- [eax]
+	mov	DWORD PTR [rbp-24], eax  	; rbp-24 <- [eax]
 	mov	eax, ebx  			; eax <- [ebx] ; eax <- 10
 	not	eax  				; eax <- ~[eax] ; eax <- 4294967285 (2^32-1-10 = ~10)
-	mov	DWORD PTR [rbp-20], eax  	; [rbp-20] <- [eax]
+	mov	DWORD PTR [rbp-20], eax  	; rbp-20 <- [eax]
 	mov	eax, 0  			; eax <- 0
 	pop	rbx  				; rbx <- stack[top-2]
 	pop	r12  				; r12 <- stack[top-1]
